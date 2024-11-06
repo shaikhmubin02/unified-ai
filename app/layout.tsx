501bff3d4ed04b43d397688273476d595270dac2
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-11-07 03:58:44 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
