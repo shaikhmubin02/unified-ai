5a2f6056c03962bee22c65fac32ffe35559192bb
Mubin <shaikhmubin572@gmail.com>
2024-12-23 16:35:52 +0530

    init

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { SignIn } from '@clerk/nextjs'
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import Stars from '@/components/ui/stars'
+
+export default function Page() {
+  return ( 
+    <div className="relative min-h-screen flex items-center justify-center bg-[#0e1011]">
+      {/* 3D Stars Background */}
+      <div className="absolute inset-0 z-0">
+        <Canvas 
+          camera={{ position: [0, 0, 1] }}
+          style={{ background: '#0e1011' }}
+        >
+          <Suspense fallback={null}>
+            <Stars />
+          </Suspense>
+        </Canvas>
+      </div>
+
+      {/* Sign In Component */}
+      <div className="relative z-10">
+        <SignIn />
+      </div>
+    </div>
+  )
+}
